3b0dfabbf11c21f1eea4619d57c19ac9bfd64b94
stmulq <65617435+stmulq@users.noreply.github.com>
2020-10-01 16:34:39 -0700

    Update index.js
    with Ming's changes

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 
 function main() {
-  process.chdir('/storage/sd');
-
-  app.use(express.static('www'));
+  
+  app.use(express.static('/storage/sd'));
 
   app.listen(9090, function() {
     console.log('Example app listening on port 9090!');
   });
 }
-window.main = main;
+
+// exchange below line to 'window.main = main;' if using node-server.html
+// as the application entry point.
+main();
